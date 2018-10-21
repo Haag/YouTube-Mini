e23af2b440d51c7ae5ed6143cd6b6db396898b81
K <codehaag@gmail.com>
2018-10-20 20:03:51 -0600

    Minor error checking added to video_detail

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyAFJeNISYs89-n-hUvNrdqQNb1ogrNfZJQ'
 
 class App extends Component{
     constructor(props) {
@@ -11,7 +14,6 @@ class App extends Component{
             videos: []
         }
         
-        const API_KEY = 'AIzaSyAFJeNISYs89-n-hUvNrdqQNb1ogrNfZJQ'
         
         YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
             this.setState({ videos: data }) // key and variable are the same ({ videos }). change data to videos
@@ -22,6 +24,7 @@ class App extends Component{
         return(
             <div>
                 <SearchBar />
+                <VideoDetail video ={this.state.videos[0]}/>
                 <VideoList videos = {this.state.videos}/>
             </div>
         )
