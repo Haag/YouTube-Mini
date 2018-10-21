bc442fe2704fbe40c806c16faa92bb86764dd94e
K <codehaag@gmail.com>
2018-10-20 21:20:53 -0600

    YT Mini is running reasonably well. 5 videos, debounced searching, and improved layout. Layout still lacks something to be desired

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,12 +4,18 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     const videoItems = props.videos.map(video => {
-        return <VideoListItem key = {video.etag} video ={video} />
+        return (
+            <VideoListItem 
+                onVideoSelect = {props.onVideoSelect}
+                key = {video.etag} 
+                video ={video} 
+            />
+        )
     })
 
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {/* {props.videos.length} */}
             {videoItems}
         </ul>
     )
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -11,21 +12,30 @@ class App extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null
         }
-        
-        
-        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-            this.setState({ videos: data }) // key and variable are the same ({ videos }). change data to videos
+        this.videoSearch('Navi')
+    }
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (data) => {
+            this.setState({ 
+                videos: data,
+                selectedVideo: data[0]
+            }) // key and variable are the same ({ videos }). change data to videos
         })
     }
 
+
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300 )
         return(
             <div>
-                <SearchBar />
-                <VideoDetail video ={this.state.videos[0]}/>
-                <VideoList videos = {this.state.videos}/>
+                <SearchBar onSearchTermChange ={videoSearch}/>
+                <VideoDetail video ={this.state.selectedVideo}/>
+                <VideoList 
+                onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                videos = {this.state.videos}/>
             </div>
         )
     }
