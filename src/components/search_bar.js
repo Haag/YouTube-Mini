ce5749375ee816c50ee18024c0c4e503ba07a32e
K <codehaag@gmail.com>
2018-10-30 11:45:51 -0600

    Added a search button that does nothing but looks better. Auto searche with debounce

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,13 +10,19 @@ class SearchBar extends Component {
     }
     render () {
         return (
-        <div className="search-bar">
-            <input 
-            placeholder = "Search"
-            value = {this.state.term}
-            onChange={ e => this.onInputChange(e.target.value)} />
-        </div>
-        
+            <div>
+                <div className="search-bar">
+                    <input 
+                    placeholder = "Search"
+                    value = {this.state.term}
+                    onChange={ e => this.onInputChange(e.target.value)} />
+                    <button className="searchbox">
+                        <div className="search-tooltip">
+                            Search
+                        </div>
+                    </button>
+                </div>
+            </div>
         )
     }
     onInputChange(term) {
@@ -24,7 +30,6 @@ class SearchBar extends Component {
         this.props.onSearchTermChange(term)
     }   
 }
-
 
 //Functional Component:
 // const SearchBar = () => {
